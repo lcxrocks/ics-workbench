85bfcf6d66accdf1dd3bce24f7e76107313418dd
tracer-nju <tracer@nju.edu.cn>
2019-12-09 14:38:30 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 52c099ad370c0444837e17b19fb7c08e            Boot ID: e73cbbbf6d494d0bbdab82c8e67aaaa0     Virtualization: kvm   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  14:38:31 up 7 days,  4:00,  4 users,  load average: 0.36, 0.27, 0.20

--- multimod/p1.c
+++ multimod/p1.c
@@ -1,6 +1,27 @@
 #include "multimod.h"
 
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
-  // TODO: implement
-  return -1;
+    char c[256]={};
+    char stra[128]={};
+    char strb[128]={};
+    char s[256]={};
+    itoa(a,stra,10);
+    itoa(b,strb,10);
+
+    int La = strlen(stra);
+    int Lb = strlen(strb);
+
+    for(int i=1;i<=La;i++)  
+        for(int j=1;j<=Lb;j++)  
+        c[i+j-1]+=(int)stra[i]*(int)strb[j];//a的第i位乘以b的第j位为积的第i+j-1位（先不考虑进位）
+
+    for(int i=1;i<=La+Lb;i++)  
+        c[i+1]+=c[i]/10,c[i]%=10;//统一处理进位  
+
+    if(c[La+Lb]) s[La+Lb]+=c[La+Lb]+'0';//判断第i+j位上的数字是不是0  
+    for(int i=La+Lb-1;i>=1;i--)  
+        s[i]+=c[i];//将整形数组转成字符串  
+    
+    return s;  
+ 
 }
