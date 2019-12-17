cea70853acf9ab9325f91f4dc068d5e726e60e0e
tracer-nju <tracer@nju.edu.cn>
2019-12-17 23:40:12 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 52c099ad370c0444837e17b19fb7c08e            Boot ID: ac19cabf7af749a0afc9436a31056e9f     Virtualization: kvm   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  23:40:13 up 13:19,  1 user,  load average: 0.38, 0.26, 0.19

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -56,6 +56,8 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
 }
 
 // C 标准库中的 setjmp/longjmp，用于控制流长跳转
+// # our buffer looks like:
+// #  rbx,rbp,r12,r13,r14,r15,rsp,rip
 int asm_setjmp(asm_jmp_buf env) {
   asm volatile(
     "movq %%rbx, 0(%[env]);"
@@ -76,32 +78,19 @@ int asm_setjmp(asm_jmp_buf env) {
 void asm_longjmp(asm_jmp_buf env, int val) {
   uint64_t _val = (uint64_t) val;
   asm volatile(
-    "movq (%[env]), %%rax;"
-    "movq 8(%[env]), %%rbx;"
-    "movq 16(%[env]), %%rcx;"
-    "movq 24(%[env]), %%rdx;"
-    "movq 32(%[env]), %%rsp;"
-    "movq 40(%[env]), %%rbp;"
-    //no rsi
-    //no rdi
-    "movq 64(%[env]), %%r8;"
-    "movq 72(%[env]), %%r9;"
-    "movq 80(%[env]), %%r10;"
-    "movq 88(%[env]), %%r11;"
-    "movq 96(%[env]), %%r12;"
-    "movq 104(%[env]), %%r13;"
-    "movq 112(%[env]), %%r14;"
-    "movq 120(%[env]), %%r15;"
-    "testq %%rax, %%rax;"
-    "movq %[val], %%rax;" //if rax!=0, ret val = val;
+    "movq %%rsi, %%rax;" //ret val
+    "movq 8(%%rdi), %%rsp;"
+    "movq 48(%%rdi), %%rsp;"
+    "pushq 56(%%rdi);"
+    "movq 0(%%rdi), %%rbx;"
+    "movq 16(%%rdi), %%r12;"
+    "movq 24(%%rdi), %%r13;"
+    "movq 32(%%rdi), %%r14;"
+    "movq 40(%%rdi), %%r15;"
+
+    "testq %%rax, %%rax;" //if rax!=0, ret val = val;
     "movq $1, %%rsi;"
     "cmove %%rsi, %%rax;" //if rax=0, ret val = 1;
-
-    "movq 128(%[env]), %%rsi;"
-    "movq %%rsi, (%%rsp);" //restore ret addr.
-    
-    "movq 48(%[env]), %%rsi;"
-    "movq 56(%[env]), %%rdi;"
     :
     :[env]"r"(env), [val]"r"(_val)  
     );
