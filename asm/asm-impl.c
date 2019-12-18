c603032c6c9a5b573d7a3c28393c31a1cbebfef5
tracer-nju <tracer@nju.edu.cn>
2019-12-19 00:59:49 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 52c099ad370c0444837e17b19fb7c08e            Boot ID: ac19cabf7af749a0afc9436a31056e9f     Virtualization: kvm   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  00:59:49 up 16:16,  1 user,  load average: 0.47, 0.53, 0.39

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -94,39 +94,41 @@ int asm_setjmp(asm_jmp_buf env) {
   );
   return 0;
 }
-// void asm_longjmp(asm_jmp_buf env, int val) {
-//   uint64_t _val = (uint64_t) val;
-//   asm volatile(
-//     "movq %%rsi, %%rax;" //ret val
-//     "movq 0x8(%%rdi), %%rsp;"
-//     "movq 0x30(%%rdi), %%rsp;"
-//     "pushq 0x38(%%rdi);"
-//     "movq (%%rdi), %%rbx;"
-//     "movq 0x10(%%rdi), %%r12;"
-//     "movq 0x18(%%rdi), %%r13;"
-//     "movq 0x20(%%rdi), %%r14;"
-//     "movq 0x28(%%rdi), %%r15;"
-
-//     "testq %%rax, %%rax;" //if rax!=0, ret val = val;
-//     "movq $1, %%rsi;"
-//     "cmove %%rsi, %%rax;" //if rax=0, ret val = 1;
-//     :
-//     :"rdi"(env), "rsi"(_val)  
-//     );
-// }
 void asm_longjmp(asm_jmp_buf env, int val) {
+  uint64_t _val = (uint64_t) val;
   asm volatile(
+    "movq %%rsi, %%rdx;" //ret val
+    "movq 8(%%rdi), %%rsp;"
+    "movq 48(%%rdi), %%rsp;"
     "movq 56(%%rdi), %%rdx;"
+    "movq (%%rdi), %%rbx;"
     "movq 16(%%rdi), %%r12;"
     "movq 24(%%rdi), %%r13;"
     "movq 32(%%rdi), %%r14;"
     "movq 40(%%rdi), %%r15;"
-    "mov %%esi, %%eax;"
-    "mov 0x30(%%rdi), %%rsp;"
-    "mov 8(%%rdi), %%rbp;"
+
+    "testq %%rax, %%rax;" //if rax!=0, ret val = val;
+    "movq $1, %%rsi;"
+    "cmove %%rsi, %%rax;" //if rax=0, ret val = 1;
     "jmpq *%%rdx;"
     :
     :"rdi"(env), "rsi"(val)  
     :"rdx"
     );
 }
+// void asm_longjmp(asm_jmp_buf env, int val) {
+//   asm volatile(
+//     "movq 56(%%rdi), %%rdx;"
+//     "movq 16(%%rdi), %%r12;"
+//     "movq 24(%%rdi), %%r13;"
+//     "movq 32(%%rdi), %%r14;"
+//     "movq 40(%%rdi), %%r15;"
+//     "mov %%esi, %%eax;"
+//     "mov 0x30(%%rdi), %%rsp;"
+//     "mov 8(%%rdi), %%rbp;"
+//     "jmpq *%%rdx;"
+//     :
+//     :"rdi"(env), "rsi"(val)  
+//     :"rdx"
+//     );
+// }
