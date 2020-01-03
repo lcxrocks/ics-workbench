6e46889ea4027f785a2e0149374aedd5f9734915
tracer-nju <tracer@nju.edu.cn>
2020-01-03 17:58:29 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 52c099ad370c0444837e17b19fb7c08e            Boot ID: 1a6cb23218584a2b943b8825a8e1afd2     Virtualization: kvm   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  17:58:30 up 8 days,  4:55,  1 user,  load average: 2.35, 2.45, 2.43

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -102,13 +102,14 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
   uint32_t in_block_addr = addr & block_offset; //获得组内地址
   uint32_t data_num = in_block_addr >>2; //因为data是用uint32存的，所以每次取都只取1整个data出来(4 Byte)
   uint32_t line_number = check_hit(gp_number,tag);
+  printf("addr\t\tdata\t\twmask\t\ttag\t\tgp_number\t\tin_block_addr\t\tdata_num\t\t\n");
+  printf("%8x\t%8x\t%8x\t%8x\t%8x\t%8x\t%8x\t\n",addr, data, wmask, tag, gp_number, in_block_addr, data_num);
   if (line_number >= 0)
     write2line(line_number, data_num, data, wmask);
   else{
     uint32_t line_miss = replace(addr, gp_number, tag);
     write2line(line_miss, data_num, data, wmask);
   }
-  
 }
 
 void init_cache(int total_size_width, int associativity_width) {
