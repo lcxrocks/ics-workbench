c1774a1a02dd91f63042f4302b3749f79abae7ae
tracer-nju <tracer@nju.edu.cn>
2020-01-03 19:01:31 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 52c099ad370c0444837e17b19fb7c08e            Boot ID: 1a6cb23218584a2b943b8825a8e1afd2     Virtualization: kvm   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  19:01:31 up 8 days,  5:58,  1 user,  load average: 2.66, 2.29, 2.21

--- cachesim/main.c
+++ cachesim/main.c
@@ -66,7 +66,7 @@ static void random_trace(void) {
     t.t.len = choose_len[ choose(sizeof(choose_len) / sizeof(choose_len[0])) ] ;
     t.t.addr = choose(MEM_SIZE) & ~(t.t.len - 1);
     t.t.is_write = choose(2);
-    if (t.t.is_write) t.data = rand();
+    if (t.t.is_write) t.data = 0;//rand();
 
     trace_exec(&t, true);
   }
