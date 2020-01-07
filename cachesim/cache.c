81729abaa99f3f444488b844a4c89b2f611b6ac8
tracer-nju <tracer@nju.edu.cn>
2020-01-08 00:53:22 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 52c099ad370c0444837e17b19fb7c08e            Boot ID: 54be745b62d64202ae9a9e3ff9ce24b8     Virtualization: kvm   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  00:53:22 up  3:34,  1 user,  load average: 0.52, 0.20, 0.17

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -142,7 +142,6 @@ void init_cache(int total_size_width, int associativity_width) {
     line[i].dirty = false;
     line[i].valid = false;
   }
-  //\033[47;31mThis is a color test.\033[0m
   printf("\033[47;31mBLOCK_WIDTH\ttotal_width_size\t associativity_width\t group_number_width\t nr_line\033[0m\n");
   printf("%8d \t %8d \t %16d \t %16d \t %12d \n",BLOCK_WIDTH,tt, as, gp, nr_line);
   //printf("finished init_cache, tt: %d, as:%d, gp:%d, nr_line:%d\n",tt,as,gp,nr_line);
