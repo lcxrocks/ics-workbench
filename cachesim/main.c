a52e8f3041ec70ad2e8ed50602467128a582dc44
tracer-nju <tracer@nju.edu.cn>
2020-01-08 01:13:26 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 52c099ad370c0444837e17b19fb7c08e            Boot ID: 54be745b62d64202ae9a9e3ff9ce24b8     Virtualization: kvm   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  01:13:26 up  3:54,  1 user,  load average: 0.50, 0.35, 0.24

--- cachesim/main.c
+++ cachesim/main.c
@@ -133,7 +133,7 @@ int main(int argc, char *argv[]) {
   parse_args(argc, argv);
   init_rand(seed);
   init_mem();
-  init_cache(20, 2);
+  init_cache(18, 2);
   replay_trace();
   display_statistic();
 
