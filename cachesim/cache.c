a5ef0cfd27ddaad7126b087e09a9b7cafe18a8cd
tracer-nju <tracer@nju.edu.cn>
2020-01-08 09:52:10 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 52c099ad370c0444837e17b19fb7c08e            Boot ID: 54be745b62d64202ae9a9e3ff9ce24b8     Virtualization: kvm   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  09:52:10 up 10:41,  1 user,  load average: 0.85, 0.48, 0.25

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -154,7 +154,9 @@ void display_statistic(void) {
   total_time = read_time + write_time;
   hit_rate = (float)hit_cnt*100/(float)total_cnt;
   time_taken = (float)miss_cycle*100/(float)cycle_cnt;
-  FILE *fp = fopen("stat.txt", "wb");
+  char tmp[4096] ;
+  FILE *fp = fopen("stat.txt","rw");
+  while(fgets(tmp,4096,fp)) continue;
   //fprintf("")
   assert(fp);
   fprintf(fp,"hit: %d / %d, miss: %d / %d, hit rate: %lf\n", hit_cnt, total_cnt, miss_cnt, total_cnt, hit_rate);
